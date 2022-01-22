bc99e7cf4985685bd0224df53278a606f2f08f2c
Mr. Joe <radics.pepe@gmail.com>
2022-01-22 11:36:49 +0100

    nagyobb változások és navmenu_v1.0_01.22

---
+++ js/main.js
@@ -0,0 +1,8 @@
+document.getElementById("menuButton").addEventListener("click", open);
+    function open(){
+        document.getElementById("navList").style.display ="flex";
+    } 
+document.getElementById("XButton").addEventListener("click", close);
+    function close(){
+        document.getElementById("navList").style.display ="none";
+    }
