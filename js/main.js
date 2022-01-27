4ac60f7921dd44aca95df5c363e6a70bfbbf8ef8
Mr. Joe <radics.pepe@gmail.com>
2022-01-27 21:50:34 +0100

    scroll button_01.27

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+//Left Navlist Button function
+
 document.getElementById("menuButton").addEventListener("click", open);
     function open(){
         document.getElementById("navList").style.display ="flex";
@@ -6,3 +8,27 @@ document.getElementById("XButton").addEventListener("click", close);
     function close(){
         document.getElementById("navList").style.display ="none";
     }
+
+// Scroll To Top Button function 
+
+let clacScrollValue = () => {
+    let ScrollProgress = document.getElementById("progress");
+    let progressValue = document.getElementById("progress-value");
+    let pos = document.documentElement.scrollTop;
+    let calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let clacScrollValue = Math.round((pos * 100)/calcHeight);
+    
+    if(pos>100){
+        ScrollProgress.style.display = "grid";
+    }
+    else{
+        ScrollProgress.style.display = "none"
+    }
+    ScrollProgress.addEventListener("click", () => {
+        document.documentElement.scrollTop = 0;
+    });
+    ScrollProgress.style.background = `conic-gradient(#818286 ${clacScrollValue}%, #81828681 ${clacScrollValue}%)`;
+};
+
+window.onscroll = clacScrollValue;
+window.onload = clacScrollValue;
