e40ad515dc5b032b75ab1c3c3c803f3f0333ac4b
Mr. Joe <radics.pepe@gmail.com>
2022-01-29 17:50:11 +0100

    mai képválasztós_01.29

--- js/main.js
+++ js/main.js
@@ -32,3 +32,14 @@ let clacScrollValue = () => {
 
 window.onscroll = clacScrollValue;
 window.onload = clacScrollValue;
+
+
+//Image function 
+
+let choseImgModel = document.querySelectorAll(".chose-img img");
+let mainImg = document.querySelector(".gallery .main-img");
+
+choseImgModel.forEach(img => img.addEventListener('click', () => {
+    mainImg.src = img.src;
+    })
+);
